c01e2c182c9f3470b4ceb1bf9bd24927343467a9
Gerson Junior <gerson.junior@gmz.solutions>
2021-02-27 00:46:45 -0300

    Ultimo dia de NLW

--- src/routes.ts
+++ src/routes.ts
@@ -2,19 +2,28 @@ import { Router } from 'express';
 import { UserController } from './controllers/UserController';
 import { SurveysController } from './controllers/SurveysController';
 import { SenMailController } from './controllers/SendMailController';
+import { AnswerController } from './controllers/AnswerController';
+import { NpsController } from './controllers/NpsController';
 
 const router = Router();
 
 const userController = new UserController();
 const surveysController = new SurveysController();
-
 const sendMailController = new SenMailController();
+const answerController = new AnswerController();
+const npsController = new NpsController();
 
-router.post("/users", userController.create)
+router.post("/users", userController.create);
 
-router.post("/surveys", surveysController.create)
-router.get("/surveys", surveysController.show)
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
-router.post("/sendMail", sendMailController.execute)
+router.post("/sendMail", sendMailController.execute);
+
+router.get("/answers/:value", answerController.execute);
+
+router.get("/nps/:survey_id", npsController.execute);
+
+
 
 export { router };
